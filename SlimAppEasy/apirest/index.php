ebf9d4096ca76e1ae02beaa60677259ad507c9fa
DarwinDurand <ddurandmorillo@gmail.com>
2017-02-26 14:27:58 -0500

    Actualizar

--- SlimAppEasy/apirest/index.php
+++ SlimAppEasy/apirest/index.php
@@ -9,6 +9,9 @@ define("SPECIALCONSTANT", TRUE);
 header("Access-Control-Allow-Origin: *");
 require 'libs/connect.php';
 require 'route/usuariosApi.php';
+require 'route/categoriasApi.php';
+require 'route/subCategoriasApi.php';
+require 'route/productosApi.php';
 
 $app->get(
     '/',function() use ($app){    	
